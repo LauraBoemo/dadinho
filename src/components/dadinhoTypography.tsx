6373843e35f3d912a546a7179d61c1ccbc5afb84
lauraboemo <lauraboemo@gmail.com>
2023-11-10 01:25:10 -0300

    remove all

--- src/components/dadinhoTypography.tsx
+++
@@ -1,8 +0,0 @@
-import { Typography, TypographyProps } from '@mui/material';
-import { styled } from '@mui/material/styles';
-
-export type DadinhoTypographyProps = TypographyProps;
-
-export const DadinhoTypography = styled(Typography)(() => ({}));
-
-export default DadinhoTypography;
--- src/theme.ts
+++
@@ -1,6 +0,0 @@
-import { createTheme } from '@mui/material/styles';
-import { theme } from './themes';
-
-const customTheme = createTheme(theme, {});
-
-export default customTheme;
