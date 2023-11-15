59933d12ffece2067733a3eb40a9fdc677d248cf
lauraboemo <lauraboemo@gmail.com>
2023-11-15 20:33:09 -0300

    Update routes

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -4,19 +4,23 @@ import AppLayout from "./layouts/app";
 import { LoginPage, LevelsPage, LevelPage } from "./pages";
 import AnswerPage from "./pages/answer";
 import RegisterPage from "./pages/register";
+import { Suspense } from "react";
+import DadinhoScreenLoader from "./components/loader/DadinhoScreenLoader";
 
 const AppRoutes = () => {
   return (
-    <Routes>
-      <Route element={<AppLayout />}>
-        <Route index element={<LoginPage />} />c
-        <Route path={PATHS.REGISTER} element={<RegisterPage />} />
-        <Route path={PATHS.LEVELS} element={<LevelsPage />} />
-        <Route path={`${PATHS.LEVELS}/:id`} element={<LevelPage />} />
-        <Route path={`${PATHS.ANSWER}/:status`} element={<AnswerPage />} />
-        {/* <Route path="*" element={<Error errorCode={ERROR_CODES.PAGE_NOT_FOUND} />} /> */}
-      </Route>
-    </Routes>
+    <Suspense fallback={<DadinhoScreenLoader layered />}>
+      <Routes>
+        <Route element={<AppLayout />}>
+          <Route index element={<LoginPage />} />c
+          <Route path={PATHS.REGISTER} element={<RegisterPage />} />
+          <Route path={PATHS.LEVELS} element={<LevelsPage />} />
+          <Route path={`${PATHS.LEVELS}/:id`} element={<LevelPage />} />
+          <Route path={`${PATHS.ANSWER}/:status`} element={<AnswerPage />} />
+          {/* <Route path="*" element={<Error errorCode={ERROR_CODES.PAGE_NOT_FOUND} />} /> */}
+        </Route>
+      </Routes>
+    </Suspense>
   );
 };
 
--- src/apis/base.ts
+++ src/apis/base.ts
@@ -1,10 +1,61 @@
 import axios from "axios";
   
+const getToken = (): string | null => {
+    try {
+      return sessionStorage.getItem("token");
+    } catch (exception) {
+      console.error("Error: sessionStorage error. Unable to read token value!!!");
+    }
+    return null;
+};
+
+const requestSuccessCallback = function (config: any): object | null {
+    let customConfig = { ...config };
+    const token = getToken();
+    if (token) {
+      customConfig = {
+        ...config,
+        headers: {
+          Authorization: `Bearer ${token}`,
+          ...customConfig.headers,
+        },
+      };
+    }
+    return customConfig;
+};
+
+const requestFailCallback = function (error: any): object | null {
+    return Promise.reject(error);
+};
+
+const responseSuccessCallbackV3 = function (config: any) {
+    return config.data;
+};
+
+const responseFailCallback = function (error: any): object | null {
+    // Do something with request error
+    const { message = "Internal Server Error", payload, response_code } = error?.response?.data || {};
+    return Promise.reject(
+      new Error(message, {
+        cause: {
+          //@ts-ignore
+          code: response_code || error?.response?.status,
+          data: payload,
+        },
+      }),
+    );
+};
+
 const baseService = axios.create({
-    baseURL: "test",
+    baseURL: "http://localhost:3030/",
 });
 
 baseService.defaults.headers.common["Content-Type"] = "application/json";
+
+// @ts-ignore
+baseService.interceptors.request.use(requestSuccessCallback, requestFailCallback);
+
+baseService.interceptors.response.use(responseSuccessCallbackV3, responseFailCallback);
 
 export default baseService;
   
