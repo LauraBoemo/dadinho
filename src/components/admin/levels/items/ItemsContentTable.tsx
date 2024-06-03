b394bd26f7d2636774bc7c889db8f86a12d70f5c
lauraboemo <lauraboemo@gmail.com>
2024-06-03 18:13:02 -0300

    Update Levels Table

--- src/components/admin/levels/items/ItemsContentTable.tsx
+++ src/components/admin/levels/items/ItemsContentTable.tsx
@@ -1,26 +1,61 @@
-import { useState } from "react";
-import { ContentBox } from "../../..";
-import BasicTable from "../../../common/DadinhoTable";
+import { useEffect, useMemo, useState } from "react";
+import { ContentBox, DadinhoBox, DadinhoLoader, DadinhoTypography } from "../../..";
+import AddItemDialog from "./AddItemDialog";
+import ItemsTable from "./ItemsTable";
+import { useGetItems } from "../../../../apis/items/useGetItems";
 import { ContentBoxTableHeader, ContentBoxTableSideContent } from "../../users/contentBoxTableConfig";
-import { AddItemDialog } from "./AddItemDialog";
 
 export const ItemsContentTable = () => {
     const [showDialog, setShowDialog] = useState(false);
+    const [searchKey, setSearchKey] = useState<string>();
+
+    const [getItems, items, itemsLoading, itemsError] = useGetItems();
+
+    useEffect(() => {
+        getItems();
+    }, []);
+
+    const rows = useMemo(() => {
+        if (!searchKey || searchKey === "") return items;
+      
+        return items?.filter((level: { name: string; }) => 
+          level.name.toLowerCase().includes(searchKey.toLowerCase())
+        );
+      }, [items, searchKey]);
 
     return (
         <>
-            <ContentBox 
-                title={
-                    <ContentBoxTableHeader title="Itens Cadastrados" />
-                } 
-                content={
-                    <BasicTable />
-                }
-                sideContent={
-                    <ContentBoxTableSideContent onButtonClick={() => setShowDialog(true)} buttonLabel="Adicionar Item" searchPlaceholder="Item" />
-                }
-            />
-            <AddItemDialog isOpen={showDialog} handleCloseDialog={() => setShowDialog(false)} />
+            {!itemsLoading && itemsError && 
+                <DadinhoTypography textAlign="center" color="error">
+                    Não foi possível carregar os níveis
+                </DadinhoTypography>
+            }
+            {itemsLoading && 
+                <DadinhoBox display="flex" sx={{ placeContent: "center" }}>
+                    <DadinhoLoader />
+                </DadinhoBox>
+            }
+            {!itemsLoading && !itemsError &&
+                <>
+                    <ContentBox 
+                        title={
+                            <ContentBoxTableHeader title="Itens Cadastrados" />
+                        } 
+                        content={
+                            <ItemsTable content={rows} />
+                        }
+                        sideContent={
+                            <ContentBoxTableSideContent
+                                onSearchKeyUpdate={(searchKeyUpdate) => setSearchKey(searchKeyUpdate)} 
+                                onButtonClick={() => setShowDialog(true)} 
+                                buttonLabel="Adicionar Item" 
+                                searchPlaceholder="Item" 
+                            />
+                        }
+                    />
+                    <AddItemDialog isOpen={showDialog} handleCloseDialog={() => setShowDialog(false)} />
+                </>
+             }
         </>
     );
 }
